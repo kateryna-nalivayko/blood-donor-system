ffb307aaf96170edcd6494c98c7e72c851d534a7
kateryna-nalivayko <katenalivayko@knu.ua>
2025-03-12 22:41:20 +0200

    Add about endpoint to provide development status

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,6 +13,11 @@ async def root():
     return {"message": "Blood Donor System API is running"}
 
 
+@app.get("/about")
+async def about():
+    return {"about": "In proccess, developing"}
+
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
